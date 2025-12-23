34a5887bfc2c0a0a40c8f158b91569f21738e4c7
Ioplop <ishk909@gmail.com>
2025-12-23 15:55:22 -0300

    [ADD] Add a simple version module to keep track of current versions

--- pdfops/__init__.py
+++ pdfops/__init__.py
@@ -1,5 +1,6 @@
 from .rectangles import Rectangle
 from .pdfmeta import PDFMeta
 from .pdfops import PDFProcessor
+from _version import __version__
 
 __all__ = ["PDFMeta", "PDFProcessor", "Rectangle"]
